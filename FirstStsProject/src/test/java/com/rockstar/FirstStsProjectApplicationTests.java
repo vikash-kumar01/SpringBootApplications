ce9fee21d382959a7dd12f7a9d3e59911c9ed7ac
vikash <vikash@gmail.com>
2022-10-25 15:18:58 +0530

    first commit

---
+++ FirstStsProject/src/test/java/com/rockstar/FirstStsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
